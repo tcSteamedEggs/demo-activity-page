a88bd752cc3316d49fd2e623165c377c9f52bbdd
tcSteamedEggs <858008221@qq.com>
2023-05-20 18:17:51 +0800

    完成tabs高亮

--- src/section/SecondSection/index.tsx
+++ src/section/SecondSection/index.tsx
@@ -1,38 +1,59 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import classNames from 'classnames'
 import CartoonImage from '../../assets/cartoon.jpg'
 import MovieImage from '../../assets/movie.png'
 import LifeImage from '../../assets/life.jpg'
 import FoodImage from '../../assets/food.jpg'
 import styles from './styles.module.scss'
 
+
 const SecondSection: FC = () => {
+    const [activeTab, setActiveTab] = useState<string>('cartoon')
+    const tabs = [
+        {
+            key: 'cartoon',
+            title: '动画',
+            src: CartoonImage
+        },
+        {
+            key: 'food',
+            title: '美食',
+            src: FoodImage
+        },
+        {
+            key: 'movie',
+            title: '电影',
+            src: MovieImage
+        },
+        {
+            key: 'life',
+            title: '生活',
+            src: LifeImage
+        }
+    ]
     return (
         <div className={styles.secondSection}>
             {/* Tabs */}
             <ul>
-                <li>动画</li>
-                <li>美食</li>
-                <li>电影</li>
-                <li>生活</li>
+                {
+                    tabs.map(tab => (
+                        <li key={tab.key} onClick={() => setActiveTab(tab.key)}>
+                            <span>{tab.title}</span>
+                            <span className={classNames(styles.line, { [styles.visible]: activeTab === tab.key })}></span>
+                        </li>
+                    ))
+                }
             </ul>
             {/* Tab Content */}
             <div>
-                <section>
-                    <h2>动画</h2>
-                    <img src={CartoonImage} alt="Cartoon" />
-                </section>
-                <section>
-                    <h2>美食</h2>
-                    <img src={FoodImage} alt="Food" />
-                </section>
-                <section>
-                    <h2>电影</h2>
-                    <img src={MovieImage} alt="Movie" />
-                </section>
-                <section>
-                    <h2>生活</h2>
-                    <img src={LifeImage} alt="Life" />
-                </section>
+                {
+                    tabs.map(tab => (
+                        <section key={tab.key}>
+                            <h2>{tab.title}</h2>
+                            <img src={tab.src} alt={tab.key} />
+                        </section>
+                    ))
+                }
             </div>
         </div>
     )
